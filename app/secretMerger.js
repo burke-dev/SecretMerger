6c06e08bdd72ff95b1035ba389be616a1d838dfd
Michael Burke <michael.burke@vivint.com>
2022-08-05 12:05:10 -0500

    refactored and set output to clipboard

--- app/secretMerger.js
+++ app/secretMerger.js
@@ -1,14 +1,36 @@
 "use strict";
-const fs = require('fs');
 
 (_ => {
-  const sectionData = _getSectionData();
-  const outputObj = _getOutputObj(sectionData);
-  _writeToFile(outputObj);
+  const outputObj = _getOutputObj();
+  _copyOutputToClipboard(outputObj);
 })();
+
+// updates the config values with the secrets values
+function _getOutputObj(){
+  const sectionData = _getSectionData();
+  if(sectionData.config && sectionData.secrets){
+    Object.keys(sectionData.secrets).forEach(key => {
+      _recursiveObjectValueMerger(sectionData.config[key], sectionData.secrets[key])
+    });
+    console.log("config and secrets merged successfully.");
+    return sectionData.config;
+  }
+  return false;
+}
+
+// if successful should copy the outputObj to the clipboard
+function _copyOutputToClipboard(outputObj){
+  if(outputObj){
+    const outputObjAsString =  `${(JSON.stringify(outputObj, null, '\t'))}\r\n`;
+    require('copy-paste').copy(outputObjAsString, _ => {
+      console.log("Copied Output to clipboard!");
+    });
+  }
+}
 
 // gets the data from the respective file and formats them into the sectionData object
 function _getSectionData(){
+  const fs = require('fs');
   const sectionData = {};
   ["config", "secrets"].forEach(sectionName => {
     const path = `${__dirname}\\..\\rawData\\${sectionName}.json`;
@@ -16,6 +38,7 @@ function _getSectionData(){
       return (_ => {
         const pathFiles = fs.readFileSync(path, 'utf8');
         sectionData[sectionName] = JSON.parse(pathFiles);
+        console.log(`${sectionName} data parsed successfully.`);
       })();
     }
     console.error(`unable to find data for ${sectionName}`);
@@ -24,44 +47,32 @@ function _getSectionData(){
   return sectionData;
 };
 
-// updates the config values with the secrets values then 
-function _getOutputObj(sectionData){
-  if(sectionData.config && sectionData.secrets){
-    Object.keys(sectionData.secrets).forEach(key => {
-      _recursiveObjectValueMerger(sectionData.config[key], sectionData.secrets[key])
-    });
-    return sectionData.config;
-  }
-  return null;
-}
-
 // digs down into the config and secret objects to find the value to be added to the config
-function _recursiveObjectValueMerger(config, secret){
+function _recursiveObjectValueMerger(config, secrets){
   config = config ?? {};
-  secret = secret ?? {};
-  if(Array.isArray(secret)){
-    let z = 0;
-    return config.map(entry => entry.includes("**") ? secret[z++] : entry);
+  secrets = secrets ?? {};
+  if(Array.isArray(secrets)){
+    return _addSecretsToConfigArray(config, secrets);
   }
-  if(typeof secret === 'object'){
-    Object.keys(secret).forEach(key => {
-      config[key] = _recursiveObjectValueMerger(config[key], secret[key]);
+  if(typeof secrets === 'object'){
+    Object.keys(secrets).forEach(key => {
+      config[key] = _recursiveObjectValueMerger(config[key], secrets[key]);
     });
     return config;
   }
-  if(typeof secret === 'string'){
-    return secret;
+  if(typeof secrets === 'string'){
+    return secrets;
   }
 }
 
-function _writeToFile(outputObj){
-  if(outputObj){
-    const outputObjAsString =  `${(JSON.stringify(outputObj, null, '\t'))}\r\n`;
-    fs.writeFile(`${__dirname}\\..\\output\\secretMerger-Output.json`, outputObjAsString, "utf8", err => {
-      if(err) {
-        return console.error(err);
-      }
-      console.log("Saved successfully!");
-    });
+// replaces the values including "**" and appends any extra secrets to the end of the array
+function _addSecretsToConfigArray(config, secrets){
+  let z = 0;
+  let configArray = config.map(entry => entry.includes("**") ? secrets[z++] : entry);
+  if(z < secrets.length){
+    for(let i = z; i < secrets.length; i++){
+      configArray.push(secrets[i]);
+    }
   }
+  return configArray;
 }
